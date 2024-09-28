4a11b8960dcd6625c4884a579f673ee67173d8c9
Nick <hello@swiftful-thinking.com>
2024-09-28 00:45:28 -0400

    Tweaks

--- Sources/SwiftfulPurchasing/Services/MockPurchaseService.swift
+++ Sources/SwiftfulPurchasing/Services/MockPurchaseService.swift
@@ -4,17 +4,20 @@
 //
 //  Created by Nick Sarno on 9/28/24.
 //
+import SwiftUI
 
-public struct MockPurchaseService: PurchaseService {
+public actor MockPurchaseService: PurchaseService {
 
-    let activeEntitlements: [PurchasedEntitlement]
+    var activeEntitlements: [PurchasedEntitlement]
+    let availableProducts: [AnyProduct]
 
-    public init(activeEntitlements: [PurchasedEntitlement] = []) {
+    public init(activeEntitlements: [PurchasedEntitlement] = [], availableProducts: [AnyProduct] = []) {
         self.activeEntitlements = activeEntitlements
+        self.availableProducts = availableProducts
     }
 
     public func getAvailableProducts() async throws -> [AnyProduct] {
-        AnyProduct.mocks
+        availableProducts
     }
 
     public func getUserEntitlements() async throws -> [PurchasedEntitlement] {
@@ -23,20 +26,43 @@ public struct MockPurchaseService: PurchaseService {
 
     public func purchaseProduct(productId: String) async throws -> [PurchasedEntitlement] {
         try? await Task.sleep(for: .seconds(1))
+        let newProduct = PurchasedEntitlement(
+            productId: productId,
+            expirationDate: Date().addingTimeInterval(7 * 24 * 60 * 60),
+            isActive: true,
+            originalPurchaseDate: .now,
+            latestPurchaseDate: .now,
+            ownershipType: .purchased,
+            isSandbox: true,
+            isVerified: true
+        )
+        activeEntitlements.append(newProduct)
         return activeEntitlements
     }
 
     public func restorePurchase() async throws -> [PurchasedEntitlement] {
         try? await Task.sleep(for: .seconds(1))
+        let newProduct = PurchasedEntitlement(
+            productId: UUID().uuidString,
+            expirationDate: Date().addingTimeInterval(7 * 24 * 60 * 60),
+            isActive: true,
+            originalPurchaseDate: .now,
+            latestPurchaseDate: .now,
+            ownershipType: .purchased,
+            isSandbox: true,
+            isVerified: true
+        )
+        activeEntitlements.append(newProduct)
         return activeEntitlements
     }
 
     public func listenForTransactions(onTransactionsUpdated: @escaping @Sendable () async -> Void) async {
-
+        await onTransactionsUpdated()
     }
 
     public func logIn(userId: String, email: String?) async throws -> [PurchasedEntitlement] {
-        activeEntitlements
+        try? await Task.sleep(for: .seconds(1))
+        return activeEntitlements
     }
 
 }
